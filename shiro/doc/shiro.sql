2baacc3f6c289141a853440497f9daab9a64a65d
御风 <54386576+hongyingwei@users.noreply.github.com>
2024-05-27 12:52:48 +0800

    shiro修改

--- shiro/doc/shiro.sql
+++ shiro/doc/shiro.sql
@@ -16,6 +16,21 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`shiro` /*!40100 DEFAULT CHARACTER SET u
 
 USE `shiro`;
 
+/*Table structure for table `role_permission` */
+
+DROP TABLE IF EXISTS `role_permission`;
+
+CREATE TABLE `role_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `permissionname` varchar(50) NOT NULL COMMENT '权限名',
+  `role_id` int(11) DEFAULT NULL COMMENT '外键关联role',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `role_permission` */
+
+insert  into `role_permission`(`id`,`permissionname`,`role_id`) values (3,'system:*',1),(4,'user:*',2),(5,'other',3);
+
 /*Table structure for table `user_role` */
 
 DROP TABLE IF EXISTS `user_role`;
@@ -29,6 +44,22 @@ CREATE TABLE `user_role` (
 /*Data for the table `user_role` */
 
 insert  into `user_role`(`id`,`rolename`) values (1,'system'),(2,'admin'),(3,'other');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户主键',
+  `username` varchar(20) NOT NULL COMMENT '用户名',
+  `password` varchar(20) NOT NULL COMMENT '密码',
+  `role_id` int(11) DEFAULT NULL COMMENT '外键关联role表',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`username`,`password`,`role_id`) values (16,'woniul','123456',1),(17,'woniu2','123456',2),(18,'woniu3','123456',3),(19,'zhang','123',2);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
